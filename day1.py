1b75379de17dc02643d259832c6b6f1c5f7cb0fc
Kwasi <ampsonasare@gmail.com>
2025-01-06 13:18:13 -0600

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,25 @@
+print("Hello World!")
+
+def add(x,y):
+    return x + y 
+"""Add 2 numbers
+x = int
+y = int
+return x + y
+"""
+results1 = add(5,6)
+print('Addition:',results1)
+
+def mult(x,y):
+    return x * y 
+"""Multiply 2 numbers
+x = int
+y = int
+return x * y
+"""
+
+results2 = mult(5,6)
+print('Multiplication:',results2)
+
+
+# I'm already a python dev :) ,and i prefer functions over basic arithmetic calculation 
