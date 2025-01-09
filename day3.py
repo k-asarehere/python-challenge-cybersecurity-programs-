6ec2789dbdecaa57741a69bc41d4da5ec3f27c41
Ampson <ampsonasare@gmail.com>
2025-01-09 00:23:02 +0000

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,11 @@
+# age checker 
+ 
+age = int(input('Enter your age: ')) # get age 
+
+
+if age >= 18: 
+    print('You are eligible to vote!')
+
+else: 
+    print('You are too young to vote!')
+
---
+++ day4.py
@@ -0,0 +1,9 @@
+
+def count(n):
+    print(n)
+
+    while n:
+        n -= 1 
+        print(n)
+
+count(10)
