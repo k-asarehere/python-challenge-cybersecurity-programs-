7cfdd314cf68d3e8d838017338968c66d2456081
Ampson <ampsonasare@gmail.com>
2025-01-12 06:41:24 +0000

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,7 @@
+# a program that takes a list of numbers and prints the sum and average 
+
+numbers = [1,2,3,4,5]
+summation = sum(numbers) # sum of list of numbers 
+average = summation / len(numbers) # average of numbers 
+print('Sum:',summation)
+print('Average:',average)
