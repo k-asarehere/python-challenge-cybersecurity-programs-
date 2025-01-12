3e6e42da2b5fc3146e08133d12381dcd73d853aa
Ampson <ampsonasare@gmail.com>
2025-01-12 06:39:28 +0000

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,5 @@
+numbers = [1,2,3,4,5]
+summation = sum(numbers) # sum of list of numbers 
+average = summation / len(numbers) # average of numbers 
+print('Sum:',summation)
+print('Average:',average)
