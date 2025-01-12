e2aadbea8f18c16a2f4bc0e6c798f1790aaa354a
Ampson <ampsonasare@gmail.com>
2025-01-12 00:40:40 -0600

    Delete day6.py

--- day6.py
+++
@@ -1,5 +0,0 @@
-numbers = [1,2,3,4,5]
-summation = sum(numbers) # sum of list of numbers 
-average = summation / len(numbers) # average of numbers 
-print('Sum:',summation)
-print('Average:',average)
