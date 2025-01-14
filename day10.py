24526362451069c06ac0eae0ac5394a805be2cd3
Ampson <ampsonasare@gmail.com>
2025-01-14 10:58:29 -0600

    Delete day10.py

--- day10.py
+++
@@ -1,7 +0,0 @@
-from math import sqrt
-
-# square root of a number 
-
-enter_number = int(input('Enter a number to find its square root: '))
-
-print(f'The square root of {enter_number} is {sqrt(enter_number)}')
