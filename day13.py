6d1e19ef675177334d32ab10cf12b9e9d21b6404
Ampson <ampsonasare@gmail.com>
2025-01-22 16:46:27 -0600

    Update day13.py

--- day13.py
+++ day13.py
@@ -4,11 +4,12 @@
 
 import requests
 
-
+# catch any input error
 try: 
     place_name = input('Enter a city name: ').title().strip()
-    api_key = 'b10ff11e4e44c72dfdb65f0f35b2c0e0'
-    site_url = 'https://api.openweathermap.org/data/2.5/weather'
+    api_key = 'b10ff11e4e44c72dfdb65f0f35b2c0e0' # get the api key so the request library can get the weather data
+    site_url = 'https://api.openweathermap.org/data/2.5/weather' # base url for api in openweathermap
+    # specifying the weather data query
     specific_query = {
         "q" : f"{place_name}",
         "appid" : "b10ff11e4e44c72dfdb65f0f35b2c0e0",
@@ -16,8 +17,9 @@ try:
 
     }
     get_api = requests.get(site_url,params=specific_query)
-    get_api_data = get_api.json()
+    get_api_data = get_api.json() # convert the weather data into json
     get_weather_info = get_api_data
+    # retrieve weather specific info
     temperature = get_weather_info['main']
     temperature_of_place = temperature['temp']
     country_or_city_name_info = get_weather_info['sys']
