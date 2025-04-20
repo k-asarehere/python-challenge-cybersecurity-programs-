1db0806496f18656ab3154c6f9909768612b331d
ampsonasare <ampsonasare@gmail.com>
2025-04-20 23:26:52 +0100

    Update hashing_algorithm.py

--- hashing_algorithm.py
+++ hashing_algorithm.py
@@ -1,8 +1,8 @@
 """a py script that hashes a user file document.
 Use Case: When you have a document file and you want to know whether it has been modified or not,
 you first have to hash the document(using this program) and after that keep the hash values(64 fixed-size string characters) somewhere. Then, as time goes on or 
-when you get suspicion that the document file has lost it's integrity, you again run it through this program. When the hash values are different(with the previous hash values you kept somewhere) it means the document file has indeed 
-been modified and has lost it's integrity.....,"""
+when you get suspicion that the document file has lost it's integrity(been modified), you again run it through this program. When the hash values are different(from the previous hash values you kept somewhere) it means the document file has indeed 
+been modified and has lost it's integrity(someone might have change some values).....,"""
 
 
 from pathlib import Path
